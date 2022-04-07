6f01b9a79a3b1478974d0a6953fd6f3ef9bef508
Diego Lavayen Alarcon <diego.lavayen.alarcon@cinch.co.uk>
2022-04-07 22:50:48 +0100

    feat: add controlled form to add a new task

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,11 +1,34 @@
+import { useState } from 'react';
 import Task from './Task';
 
-const TaskList = ({ tasks, updateTask }) => {
+const TaskList = ({ tasks, updateTask, addTask }) => {
+  const [task, setTask] = useState('');
+
+  const handleInputChange = (event) => {
+    setTask(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    addTask(task);
+    setTask('');
+  };
+
   return (
     <ul>
       {tasks.map((task) => (
         <Task key={task.id} task={task} updateTask={updateTask} />
       ))}
+      <li>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            name="task"
+            value={task}
+            onChange={handleInputChange}
+          />
+        </form>
+      </li>
     </ul>
   );
 };
